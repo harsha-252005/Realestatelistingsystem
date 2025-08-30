8a15fd8d22ab96c17a62a5e3b9506eed9370549c
harsha-252005 <hv200525@gmail.com>
2025-08-30 05:17:23 +0000

    commit

---
+++ springapp/src/main/java/com/examly/springapp/model/Property.java
@@ -0,0 +1,62 @@
+package com.examly.springapp.model;
+
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(max = 100)
+    private String title;
+    @NotBlank
+    @Size(max = 500)
+    private String description;
+    @NotBlank 
+    @Positive
+    private double price;
+    @NotBlank
+    @Positive
+    private int bedrooms;
+    @NotBlank
+    @Positive
+    private double bathrooms;
+    @NotBlank
+    @Positive
+    private double area;
+    @NotBlank
+    private String address;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String state;
+    @NotBlank
+    @Pattern(regexp = "^[0-9{5(?:-[0-9]{4})?$", message = "Invalid ZIP code format")
+    private String zipcode;
+    @NotBlank
+    private String propertyType;
+    @NotNull
+    private LocalDate listingDate;
+    @NotNull
+    private boolean isAvailable;
+
+}
---
+++ springapp/src/main/java/com/examly/springapp/service/PropertyService.java
@@ -0,0 +1,25 @@
+package com.examly.springapp.service;
+
+import java.util.List;
+import java.util.Optional;k.stereotype.Service;
+
+import com.examly.springapp.model.Property;
+import com.examly.springapp.repository.PropertyRepository;
+
+public class PropertyService {
+    private final PropertyRepository propertyRepository;
+    
+    public PropertyService(PropertyRepository propertyRepository){
+        this.propertyRepository=propertyRepository;
+    }
+
+    public Property addProperty(Property property){
+        return propertyRepository.save(property);
+    }
+    public List<Property> getAllProperties(){
+        return propertyRepository.findAll();
+    }
+    public Optional<Property>getPropertyById(Long id){
+        return propertyRepository.findById(id);
+    }
+}
